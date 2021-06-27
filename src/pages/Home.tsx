4f64cd6bbecc10cc58f8c9a0a911eb9c40678fb3
Caio Falcão <caio.falcao@live.com>
2021-06-27 12:19:20 -0300

    finalizado encerrar sala

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -41,6 +41,11 @@ export function Home() {
             return;
         }
 
+        if (roomRef.val().endedAt) {
+            alert('Sala já encerrada');
+            return;
+        }
+
         history.push(`/rooms/${roomCode}`);
     }
 
